6222dabe3a077d86020c570e27ae83698eac5b0d
ethanga12 <66858278+ethanga12@users.noreply.github.com>
2020-10-01 15:32:28 -0400

    added required pips .txt file

---
+++ getDropScrape.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import time
+import csv
+import urllib
+import requests
+import pandas
+import httplib2
