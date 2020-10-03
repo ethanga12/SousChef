71fa6cf85019c239209fb265c5fb7fd92b984d57
ethanga12 <66858278+ethanga12@users.noreply.github.com>
2020-10-03 02:08:30 -0400

    updated computer vision w more formally logged results (more info in channel)

--- computerVision/visionTrial.py
+++ computerVision/visionTrial.py
@@ -1,6 +1,7 @@
 import os, io
 from google.cloud import vision
 from google.cloud.vision import types
+import pandas as pd
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'/Users/ethanasis/Desktop/SousChef/eco-watch-291322-394e56de3abb.json'
 client = vision.ImageAnnotatorClient()
@@ -12,7 +13,15 @@ with io.open(os.path.join(FOLDER_PATH, FILE_NAME), 'rb') as image_file:
     content = image_file.read()
 
 image = vision.types.Image(content=content)
-
 response = client.text_detection(image=image)
+texts = response.text_annotations
 
-print(response)
+df = pd.DataFrame(columns=['description'])
+for text in texts:
+    df = df.append(
+        dict(
+            description = text.description
+        ),
+        ignore_index = True
+    )
+df.to_csv('/Users/ethanasis/Desktop/Projects/SousChef/computerVision/bigReceipt3.csv')
