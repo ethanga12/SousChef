1d708f8b8fb51cf48ccb50f8658a1a730c7ebb2a
Lucas Gelfond <lucasgelfond@gmail.com>
2020-10-04 10:43:31 -0400

    added keys and got layering correct

--- sous-chef/src/App.js
+++ sous-chef/src/App.js
@@ -8,20 +8,20 @@ function App() {
   return (
     <div className="App">
       <div id="cal-sidebar">
-        <Sidebar width={100} height={"100vh"}>
-          <CalendarDate date="10" />
-          <CalendarDate date="11" />
-          <CalendarDate date="12" />
-          <CalendarDate date="13" />
-          <CalendarDate date="14" />
-          <CalendarDate date="15" />
-          <CalendarDate date="16" />
+        <Sidebar width={100}>
+          <CalendarDate key="35" date="10" />
+          <CalendarDate key="36" date="11" />
+          <CalendarDate key="30" date="12" />
+          <CalendarDate key="39" date="13" />
+          <CalendarDate key="41" date="14" />
+          <CalendarDate key="42" date="15" />
+          <CalendarDate key="45" date="16" />
         </Sidebar>
       </div>
-      <div id="app-content">
-        <Responsive />
-        <Responsive />
-        <Responsive />
+      <div id="app-content" >
+        <Responsive  key="47" />
+        <Responsive key="48" />
+        <Responsive key="49" />
       </div>
     </div>
   );
